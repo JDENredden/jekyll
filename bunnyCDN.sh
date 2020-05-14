fd86fe9ff7f7e95161163ab90a6916b310821ba2
jden redden <iam@jden.me>
2020-05-14 22:08:05 +0930

    bunnyCDN

---
+++ bunnyCDN.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl --include \
+     --request POST \
+     --header "Content-Type: application/xml" \
+     --header "Accept: application/json" \
+     --header "AccessKey: 3aa7bf31-dd7e-41fa-9c99-c1198e7cf332c9e030b0-ed6c-4e18-bf92-323f64103c96" \
+     --header "Content-Length: 0" \
+     'https://bunnycdn.com/api/pullzone/132574/purgeCache'
